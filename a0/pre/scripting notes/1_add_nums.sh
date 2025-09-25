06a2ebec3328a2a845fc82a2c3a223731127db95
sarimbaig01 <sarim.baig@gmail.com>
2025-09-25 23:11:23 +0100

    SS assignments

--- a0/pre/scripting notes/1_add_nums.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-num1=3
-num2=2
-num3=8
-echo $num1+$num2+$num3 
--- cpp/helloworld.cpp
+++
@@ -1,8 +0,0 @@
-// helloworld.cpp
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
